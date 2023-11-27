1c829314f45737433f821fa4b45797980641da77
Colin LeFevre <colin.lefevre.school@gmail.com>
2023-11-27 14:40:21 -0500

    icon updates

--- screens/ViewData.js
+++ screens/ViewData.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faFaceSmile } from "@fortawesome/free-solid-svg-icons/faFaceSmile";
+import { faFaceMeh } from "@fortawesome/free-solid-svg-icons/faFaceMeh";
+import { faFaceFrown } from "@fortawesome/free-solid-svg-icons/faFaceFrown";
 
 import { fetchData } from "../modules/supabase";
 import { globalStyles } from "../modules/globalStyles";
@@ -22,11 +26,12 @@ export default function ViewData() {
             <Text>Supabase Data:</Text>
             <FlatList
                 data={data}
-                style={globalStyles.flatList}
+                style={styles.flatList}
                 renderItem={({ item }) => (
                     <FlatListEntry
                         mood_time={item.created_at}
                         mood_name={item.mood_name}
+                        mood_num={item.mood_num}
                     />
                 )}
                 keyExtractor={(item) => item.id.toString()}
@@ -35,9 +40,24 @@ export default function ViewData() {
     );
 }
 
-function FlatListEntry({ mood_name, mood_time }) {
+function FlatListEntry({ mood_name, mood_time, mood_num }) {
+    let emojiName;
+    let emojiColor;
+
+    if (mood_num == 1) {
+        emojiName = faFaceSmile;
+        emojiColor = "green";
+    } else if (mood_num == 2) {
+        emojiName = faFaceMeh;
+        emojiColor = "blue";
+    } else if (mood_num == 3) {
+        emojiName = faFaceFrown;
+        emojiColor = "red";
+    }
+
     return (
         <View style={styles.flatListEntry}>
+            <FontAwesomeIcon icon={emojiName} size={24} color={emojiColor} />
             <Text style={styles.moodName}>{mood_name}</Text>
             <Text style={styles.moodTime}>{formatTime(mood_time)}</Text>
         </View>
@@ -59,15 +79,21 @@ function formatTime(timestamp) {
 }
 
 const styles = StyleSheet.create({
+    flatList: {
+        flex: 1,
+        marginHorizontal: 5,
+    },
     flatListEntry: {
         flexDirection: "row",
-        gap: 10,
-        justifyContent: "flex-start",
+        flex: 1,
+        justifyContent: "space-between",
     },
     moodName: {
         color: "black",
+        fontSize: 20,
     },
     moodTime: {
         color: "blue",
+        fontSize: 15,
     },
 });
